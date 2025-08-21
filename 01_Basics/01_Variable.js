c206c8087acf7a048eb7ce2cd843c8d5ccb5282a
SudhirRaut-QA <sudhirraut12@gmail.com>
2025-08-21 10:27:13 +0000

    JS Variables

---
+++ 01_Basics/01_Variable.js
@@ -0,0 +1,22 @@
+const userID=2441
+let userName="Sudhirr"
+var uPassword="1234"
+state="MH"
+let city;
+
+// userID = 2 // not allowed
+
+
+userName = "Hitesh"
+uPassword = "43210"
+state = "Bengaluru"
+
+console.log(userID);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([userID, userName,uPassword, state, city])
